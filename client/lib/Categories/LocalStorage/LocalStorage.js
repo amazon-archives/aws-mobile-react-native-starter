13264cfbcdbb5783c977125ecee520df1330add5
Manuel Iglesias <manuelig@amazon.com>
2017-09-27 14:34:16 -0700

    Include license

--- client/lib/Categories/LocalStorage/LocalStorage.js
+++ client/lib/Categories/LocalStorage/LocalStorage.js
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
+ * the License. A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 import { AsyncStorage } from 'react-native';
 
 let queue = Promise.resolve();
@@ -34,7 +46,7 @@ class MemoryStorage {
 
   /**
    * Stores an item in the local storage
-   * 
+   *
    * @param {string} key 
    * @param {string} value 
    * @returns {string}
@@ -50,7 +62,7 @@ class MemoryStorage {
 
   /**
    * Retrieves an item from the local storage
-   * 
+   *
    * @param {string} key 
    * @returns {string}
    */
@@ -61,7 +73,7 @@ class MemoryStorage {
 
   /**
    * Removes an item from local storage
-   * 
+   *
    * @param {string} key 
    */
   removeItem(key) {
